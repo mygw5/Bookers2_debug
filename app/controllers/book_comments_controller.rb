28b32a68367ac8216b81bd2d412864bea3bbb873
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-17 06:31:06 +0000

    [add]非同期通信化エラーメッセージ追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,11 @@
 class BookCommentsController < ApplicationController
   def create
     @book=Book.find(params[:book_id])
-    comment=current_user.book_comments.new(book_comment_params)
-    comment.book_id=@book.id
-    comment.save
+    @comment=current_user.book_comments.new(book_comment_params)
+    @comment.book_id=@book.id
+    unless @comment.save
+      render 'error'
+    end
     #redirect_to request.referer
   end
 
