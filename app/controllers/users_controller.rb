59354daaa959a6bb3e9894a545848b7d723b7d32
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-18 04:39:19 +0000

    [add] 投稿数追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
@@ -15,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
