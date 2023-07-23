33e38f224530a08e19ca6559d35e9040d9abfa86
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-23 04:48:50 +0000

    [fix] migrationの綴り間違い修正

--- db/migrate/20230722002735_create_group_users.rb
+++ db/migrate/20230722002735_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :group_users do |t|
-      t.reference :user, foreign_key: true
-      t.reference :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
